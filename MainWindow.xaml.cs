907b81298fcb7ac5125e4bdfe89b9e6f5d5e8cd1
Igor Kuvychko <igor.kuvychko@gmail.com>
2025-06-15 20:55:57 -0700

    Import cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using Google.OrTools.LinearSolver;
 using Google.OrTools.Sat;
 
 namespace TSPlayground;
